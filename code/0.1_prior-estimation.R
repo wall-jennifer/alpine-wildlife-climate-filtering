bc7e4499a667162ded24e33add34d4afca7e3ca0
Jen Wall <74634137+wall-jennifer@users.noreply.github.com>
2025-10-09 19:52:10 -0400

    Add files via upload

---
+++ code/0.1_prior-estimation.R
@@ -0,0 +1,43 @@
+# Prior development and estimation
+
+# Simulate priors 
+res <- 100
+x <- seq(-3,3, length.out = res)
+
+n <- 1000
+beta0 <- rlogis(n, 0, 1)      # y-intercept
+beta1 <- rnorm(n, -0.25, 0.5) # weakly informative prior
+
+
+# Check the histogram of the y-intercept (should be relatively uniform, as opposed to beta1)
+hist(plogis(beta0))
+hist(beta1)
+
+# For loop to see how this would interact in the model
+p <- matrix(NA, n, res)
+
+for (j in 1:res){
+  p[,j] <- beta0 + beta1 * x[j]
+}
+
+boxplot(plogis(p), names = round(x,digits = 1), outline = F)
+# as you can see by the boxplot, this distribution would result in a prior with a negative
+# slope that allows for all possible values, but is more concentrated between 0.3 and 0.7
+
+# Here are the priors we used for each species:
+# Copi
+evi <- rnorm(n, 0.25, 0.15)
+ros <- rnorm(n, 0.25, 0.15)
+terrain <- rnorm(n, 0.5, 0.25)
+temp <- rnorm(n, 0.3, 0.5)
+
+# Homa
+terrain <- rnorm(n, 0.5, 0.25)
+minwin <- rnorm(n, 0.25, 0.25)
+evi <- rnorm(n, -0.25, 0.25)
+sumwin <- rnorm(n, -0.15, 0.25) # weakly negative trend, might be non-linear but mechanistically wouldn't make sense??
+
+
+
+
+
